3ef7f95a5e5e382900a80aae9ff92354f244bb19
codes71 <thuraheinsyh@gmail.com>
2024-12-28 10:45:59 +0530

    BottomNavFinal

--- BottomNavClass/app/src/main/java/com/example/bottomnavclass/MainActivity.kt
+++ BottomNavClass/app/src/main/java/com/example/bottomnavclass/MainActivity.kt
@@ -3,18 +3,35 @@ package com.example.bottomnavclass
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        loadFragment(HomeFragment())
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        bottomNavigationView.setOnNavigationItemSelectedListener{ item ->
+
+            val fragment : Fragment = when(item.itemId){
+                R.id.homeIcon_nav -> HomeFragment()
+                R.id.callIcon_nav -> CallFragment()
+                R.id.photoIcon_nav -> PhotoFragment()
+                R.id.messageIcon_nav ->MessageFragment()
+                R.id.profileIcon_nav -> ProfileFragment()
+                else -> HomeFragment()
+            }
+            loadFragment(fragment)
+            true
         }
+    }
+
+    private fun loadFragment(homeFragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_graph, homeFragment)
+            .commit()
     }
 }
